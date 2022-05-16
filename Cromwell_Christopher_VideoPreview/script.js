7d96b9f39c52c7893574077fc4e1fd277e1a3278
Christopher Cromwell <victoryhp@ymail.com>
2022-05-16 11:03:23 -0500

    22-05-16

---
+++ Cromwell_Christopher_VideoPreview/script.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+var x = document.getElementById("video1");
+
+function enableControls() {
+    x.controls = true;
+}
+
+
+function disableControls() {
+    x.controls = false;
+}
+
